d130d1d35b236c6b7dfb3b67a2696e7524a947d9
Elise Elf <elise.elf@hotmail.com>
2026-09-14 09:44:46 +0200

    andra committen

--- src/main/java/me/ellebelle/Rectangle.java
+++ src/main/java/me/ellebelle/Rectangle.java
@@ -1,4 +1,12 @@
 package me.ellebelle;
 
 public class Rectangle {
+    // dessa kallas för fält/field. Detta är data-delen av objektorientering.
+    // vi vill ha dessa privata. Den enda som kan skriva till dessa (initiera
+    //  variablerna) är endast kod i den egna klassen.
+    private int width;
+    private int height;
+    private Color color;
 }
+
+record Color(int red, int green, int blue) {}
